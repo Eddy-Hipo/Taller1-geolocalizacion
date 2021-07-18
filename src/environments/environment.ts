5341763013eafd9eb73508af69b56d8b52035890
Eddy Hipo <66134986+Eddy-Hipo@users.noreply.github.com>
2021-07-17 20:33:03 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyBpGTqGUrNu4rNqUA0W73p7gL35Q_Qn9oY",
-    authDomain: "proyecto-ionic-prueba.firebaseapp.com",
-    projectId: "proyecto-ionic-prueba",
-    storageBucket: "proyecto-ionic-prueba.appspot.com",
-    messagingSenderId: "849331438018",
-    appId: "1:849331438018:web:732b827450d7a3b9ead65b",
-    measurementId: "G-5MKV70R1KD"
+    apiKey: "<apiKeyFirebase>",
+    authDomain: "<apiKeyFirebase>",
+    projectId: "<apiKeyFirebase>",
+    storageBucket: "<apiKeyFirebase>",
+    messagingSenderId: "<apiKeyFirebase>",
+    appId: "<apiKeyFirebase>",
+    measurementId: "<apiKeyFirebase>"
   }
 };
 
